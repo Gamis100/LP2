497e69ea63cb97de6edfec0929799f79f74084df
Gamis777 <42852437+Gamis777@users.noreply.github.com>
2019-03-28 12:25:06 -0500

    Update busqueda binaria iterativa.cpp

--- busqueda binaria iterativa.cpp
+++ busqueda binaria iterativa.cpp
@@ -11,13 +11,13 @@ int busqueda_binaria (int arr[], int l, int r, int x)
     if (arr[m] < x)  
         l = m + 1;  
   
-// Si x es más pequeño, ignora la mitad derecha 
+// Si x es mas peque, ignora la mitad derecha 
     else 
          r = m - 1;  
   } 
   
  
-// si llegamos aquí, entonces el elemento no estaba presente
+// si llegamos aqui, entonces el elemento no estaba presente
   return -1;  
 } 
   
@@ -27,7 +27,7 @@ int main(void)
    int n = sizeof(arr)/ sizeof(arr[0]); 
    int x = 10; 
    int result = busqueda_binaria (arr, 0, n-1, x); 
-   (result == -1)? printf("Element is not present in array") 
-                 : printf("Element is present at index %d", result); 
+   (result == -1)? printf("El elemento no esta presente en la matriz") 
+                 : printf("El elemento esta presente en el indice %d", result); 
    return 0; 
 } 
