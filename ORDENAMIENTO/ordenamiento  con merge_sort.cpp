2172cd6481bee2744efc3da7a69cece0f4f777bd
Gamis777 <42852437+Gamis777@users.noreply.github.com>
2019-04-04 11:49:58 -0500

    Update ordenamiento  con merge_sort.cpp

--- ORDENAMIENTO/ordenamiento  con merge_sort.cpp
+++ ORDENAMIENTO/ordenamiento  con merge_sort.cpp
@@ -77,7 +77,7 @@ void Insertion_Sort(int *A, int n){
 	
 }
 
-void merge(int arr[], int l, int m, int r) 
+void merge(int array[], int l, int m, int r) 
 { 
     int i, j, k; 
     int n1 = m - l + 1; 
@@ -86,9 +86,9 @@ void merge(int arr[], int l, int m, int r)
     int L[n1], R[n2]; 
 
     for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
+        L[i] = array[l + i]; 
     for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
+        R[j] = array[m + 1+ j]; 
   
     
     i = 0; 
@@ -98,12 +98,12 @@ void merge(int arr[], int l, int m, int r)
     { 
         if (L[i] <= R[j]) 
         { 
-            arr[k] = L[i]; 
+            array[k] = L[i]; 
             i++; 
         } 
         else
         { 
-            arr[k] = R[j]; 
+            array[k] = R[j]; 
             j++; 
         } 
         k++; 
@@ -112,7 +112,7 @@ void merge(int arr[], int l, int m, int r)
     
     while (i < n1) 
     { 
-        arr[k] = L[i]; 
+        array[k] = L[i]; 
         i++; 
         k++; 
     } 
@@ -120,24 +120,24 @@ void merge(int arr[], int l, int m, int r)
    
     while (j < n2) 
     { 
-        arr[k] = R[j]; 
+        array[k] = R[j]; 
         j++; 
         k++; 
     } 
 } 
   
 
-void mergeSort(int arr[], int l, int r) 
+void mergeSort(int array[], int l, int r) 
 { 
     if (l < r) 
     { 
         int m = l+(r-l)/2; 
   
        
-        mergeSort(arr, l, m); 
-        mergeSort(arr, m+1, r); 
+        mergeSort(array, l, m); 
+        mergeSort(array, m+1, r); 
   
-        merge(arr, l, m, r); 
+        merge(array, l, m, r); 
     } 
 } 
 
