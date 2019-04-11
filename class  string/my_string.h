353d3b8c660611630f04e754ed4aa382b9b3947e
Gamis777 <42852437+Gamis777@users.noreply.github.com>
2019-04-11 14:06:40 -0500

    Add files via upload

---
+++ class  string/my_string.h
@@ -0,0 +1,33 @@
+#ifndef TEST_H
+#define TEST_H
+#include<iostream>
+using  namespace std;
+class  String 
+{
+	private:
+		char  *data;
+		int length;
+		
+	public:
+		String();
+        String(char c);
+        String(const char *str=NULL);
+        String(const String &str);
+		~String();
+		
+		friend ostream &operator<<(ostream & os, const String &str);
+		char operator[](int j) const;
+        char &operator[](int j);
+        String &operator=(const String &str);
+        String &operator+=(const String &str);
+        friend String operator+(const String &lhs, const String &rhs);
+        friend String operator+(const String &lhs, char rhs);
+        friend String operator+(const String &lhs, const char *rhs);
+        friend String operator+(char lhs, const String &rhs);
+        friend String operator+(const char *lhs, const String &rhs);
+			
+};
+
+			
+
+
